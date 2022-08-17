0bad454bc496ee501ca3b20af8964333cd4822e9
Samuel Calderon <samuel.calderon@uarm.pe>
2022-08-17 12:09:57 -0500

    setup basic testing

---
+++ tests/testthat/test-choose_years.R
@@ -0,0 +1,13 @@
+test_that("check_years() works inside choose_years()", {
+    df <- sep()
+    expect_error(df |> choose_years(2011))
+    expect_error(df |> choose_years(current_year() + 1))
+})
+
+test_that("choose_years() returns expected objects", {
+    df <- sep() |> choose_years()
+    expect_s3_class(df, "sep_df")
+    expect_type(get_query(df)$years, type = "list")
+    expect_type(get_query(df)$years[[1]], type = "integer")
+})
+
