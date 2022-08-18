c96cc9a82094fafed7a4db50c0d84eda7ab2bc5e
calderonsamuel <samuel.calderon@uarm.pe>
2022-08-18 16:40:26 -0500

    add method to show expanded query

--- R/consultar.R
+++ R/consultar.R
@@ -3,11 +3,8 @@ consultar <- function(x) UseMethod("consultar")
 
 #' @export
 consultar.sep_df <- function(x) {
-    query <- get_query(x) |> purrr::flatten()
-    check_years(query$years)
-    translated <- sep_params_translate(query)
     request <- get_req_url(x)
-    expanded <- do.call(tidyr::expand_grid, translated)
+    expanded <- expand_query(x)
 
     expanded |>
         split_by_row() |>
@@ -16,7 +13,7 @@ consultar.sep_df <- function(x) {
 }
 
 consulta_individual <- function(x, request) {
-    lista <- as.list(x)
+    lista <- as.list(x) |> empty_str_to_last()
     year <- lista$y
     params <- purrr::list_modify(.req = request, lista)
     do.call(httr2::req_url_query, params) |>
@@ -42,4 +39,25 @@ inherit_attr <- function(new_df, inherit_from) {
     attr(new_df, "query") <-  attr(inherit_from, "query")
     attr(new_df, "req_url") <-  attr(inherit_from, "req_url")
     new_df
+}
+
+#' @noRd
+expand_query <- function(x) UseMethod("expand_query")
+
+#' @export
+expand_query.sep_df <- function(x) {
+    query <- get_query(x) |> purrr::flatten()
+    check_years(query$years)
+    translated <- sep_params_translate(query)
+    expanded <- do.call(tidyr::expand_grid, translated)
+    expanded
+}
+
+empty_str_to_last <- function(x) {
+    empty_str <- purrr::keep(x, ~.x == "")
+    empty_str_name <- names(empty_str)
+    empty_str_value <- empty_str[[1]]
+    remaining <- purrr::discard(x, ~.x == "")
+    remaining[[empty_str_name]] <- empty_str_value
+    remaining
 }
