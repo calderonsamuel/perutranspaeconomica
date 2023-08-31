f334c8839bcc99f859acaf2e070e8a1d5c7c6855
Samuel Calderon <samuel.calderon@uarm.pe>
2023-08-31 17:11:12 -0500

    make sep_params() a function

--- R/sep_params.R
+++ R/sep_params.R
@@ -1,36 +1,38 @@
-sep_params <- list(
-    "y" = "periodo",
-    "1" = "nivel",
-    "2" = "sector",
-    "3" = "pliego",
-    "4" = "unidad_ejecutora",
-    "5" = "departamento",
-    "6" = "provincia",
-    "7" = "municipalidad",
-    "37" = "goblocal_o_manc",
-    "36" = "mancomunidad",
-    "24" = "generica",
-    "25" = "subgenerica",
-    "26" = "detalle_subgenerica",
-    "27" = "especifica",
-    "28" = "detalle_especifica",
-    "14" = "fuente_financiamiento",
-    "15" = "rubro",
-    "17" = "tipo_de_recurso",
-    "21" = "departamento_meta",
-    "30" = "categoria_presupuestal",
-    "31" = "producto",
-    "32" = "actividad",
-    "13" = "meta",
-    "8" = "funcion",
-    "33" = "division_funcional",
-    "34" = "grupo_funcional",
-    "22" = "trimestre",
-    "23" = "mes"
-)
+sep_params <- function() {
+    list(
+        "y" = "periodo",
+        "1" = "nivel",
+        "2" = "sector",
+        "3" = "pliego",
+        "4" = "unidad_ejecutora",
+        "5" = "departamento",
+        "6" = "provincia",
+        "7" = "municipalidad",
+        "37" = "goblocal_o_manc",
+        "36" = "mancomunidad",
+        "24" = "generica",
+        "25" = "subgenerica",
+        "26" = "detalle_subgenerica",
+        "27" = "especifica",
+        "28" = "detalle_especifica",
+        "14" = "fuente_financiamiento",
+        "15" = "rubro",
+        "17" = "tipo_de_recurso",
+        "21" = "departamento_meta",
+        "30" = "categoria_presupuestal",
+        "31" = "producto",
+        "32" = "actividad",
+        "13" = "meta",
+        "8" = "funcion",
+        "33" = "division_funcional",
+        "34" = "grupo_funcional",
+        "22" = "trimestre",
+        "23" = "mes"
+    )
+}
 
 sep_params_translate <- function(params_list) {
-    sep_params |>
+    sep_params() |>
         purrr::keep(~.x %in% names(params_list)) |>
         purrr::map(~params_list[[.x]]) |>
         purrr::map(~ifelse(.x == "todos", "", .x))
