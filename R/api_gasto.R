bc054e8105b83a635b8ba8cfd930ae00acaaf3db
Samuel Calderon <samuel.calderon@uarm.pe>
2023-09-12 11:06:03 -0500

    api_gasto() ahora permite escoger modulo de ingreso

--- R/api_gasto.R
+++ R/api_gasto.R
@@ -4,10 +4,27 @@
 #'
 #' @return chr. La URL base de una API de seguimiento a la ejecucion del gasto
 #'
-api_gasto <- function(actualizacion = "mensual") {
-    tipo_act <- match.arg(actualizacion, c("mensual", "diaria"))
-    switch(tipo_act,
-        mensual = "https://apps5.mineco.gob.pe/transparencia/mensual/Navegar_6.aspx",
-        diaria = "https://apps5.mineco.gob.pe/transparencia/Navegador/Navegar_7.aspx"
+api_gasto <- function(actualizacion = "mensual", modulo = "gasto") {
+
+    if (!modulo %in% c("gasto", "ingreso")) {
+        cli::cli_abort("{.code modulo} debe ser {.str gasto} o {.str ingreso}")
+    }
+
+    if (! actualizacion %in% c("diaria", "mensual")) {
+        cli::cli_abort("{.code actualizacion} debe ser {.str diaria} o {.str mensual}")
+    }
+
+    api <- list(
+        gasto = list(
+            diaria = "https://apps5.mineco.gob.pe/transparencia/Navegador/Navegar_7.aspx",
+            mensual = "https://apps5.mineco.gob.pe/transparencia/mensual/Navegar_6.aspx"
+        ),
+        ingreso = list(
+            diaria = "https://apps5.mineco.gob.pe/transparenciaingresos/Navegador/Navegar_6.aspx",
+            mensual = "https://apps5.mineco.gob.pe/transparenciaingresos/mensual/Navegar_6.aspx"
+        )
     )
+
+    api[[modulo]][[actualizacion]]
+
 }
--- tests/testthat/test-api_gasto.R
+++ tests/testthat/test-api_gasto.R
@@ -1,3 +1,20 @@
-test_that("api_gasto() funciona", {
-    expect_type(api_gasto(), type = "character")
+test_that("api_gasto() funciona por defecto", {
+    api_gasto() %>%
+        expect_type(type = "character")
+})
+
+test_that("api_gasto() permite especificar modulo y actualizacion", {
+    api_gasto(actualizacion = "diaria") %>%
+        expect_type(type = "character")
+
+    api_gasto(modulo = "ingreso") %>%
+        expect_type(type = "character")
+})
+
+test_that("api_gasto() genera errores cuando debe" , {
+    api_gasto(modulo = "moduloInexistente") %>%
+        expect_error(regexp = "modulo")
+
+    api_gasto(actualizacion = "actualizacionInexistente") %>%
+        expect_error(regexp = "actualizacion")
 })
