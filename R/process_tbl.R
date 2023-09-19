41b5b7109d42fdb3ed6ad0292172e48410192538
Samuel Calderon <samuel.calderon@uarm.pe>
2023-09-19 17:03:41 -0500

    rm two dplyr calls

--- R/process_tbl.R
+++ R/process_tbl.R
@@ -3,11 +3,12 @@ process_tbl <- function(tbl) {
     num_columns <- c("pia", "pim", "certificacion", "compromiso_anual",
                      "atencion_de_compromiso_mensual", "devengado", "girado", "avance_percent")
     tbl |>
-        # janitor::remove_empty("cols") |>
-        dplyr::select(-1L) |>
-        dplyr::distinct(dplyr::across(.cols = dplyr::everything())) |>
+        remove_first_column() |>
+        unique() |> # keep only unique permutations, returns data.frame
         dplyr::filter(!is.na(readr::parse_number(.data$certificacion))) |>
         separate_cod_desc() |>
         dplyr::mutate(dplyr::across(.cols = tidyselect::all_of(num_columns), .fns = readr::parse_number)) |>
         suppressWarnings()
 }
+
+remove_first_column <- function(x) x[ , -1, drop = FALSE]
