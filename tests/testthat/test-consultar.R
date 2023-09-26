22d38a980b0a0fb443f9a945575b515adf908ba9
Samuel Calderon <samuel.calderon@uarm.pe>
2023-09-26 11:34:41 -0500

    fix check() error

--- tests/testthat/test-consultar.R
+++ tests/testthat/test-consultar.R
@@ -16,7 +16,7 @@ test_that("consultar() sirve para todos los elementos de query()", {
         elegir_periodo_anual(periodo = current_year())
     
     expect_s3_class(df %>% elegir_quien_gasta(nivel = "todos") %>% consultar() %>% suppressMessages(), "data.frame")
-    expect_s3_class(df %>% elegir_como_se_estructura(generica = "todos") %>% consultar() %>% suppressMessages(), "data.frame")
+    expect_s3_class(df %>% elegir_como_se_estructura_gasto(generica = "todos") %>% consultar() %>% suppressMessages(), "data.frame")
     expect_s3_class(df %>% elegir_con_que_se_financia(fuente_financiamiento = "todos") %>% consultar() %>% suppressMessages(), "data.frame")
     expect_s3_class(df %>% elegir_cuando_se_hizo_gasto(trimestre = "todos") %>% consultar() %>% suppressMessages(), "data.frame")
     expect_s3_class(df %>% elegir_en_que_se_gasta(categoria_presupuestal = "todos") %>% consultar() %>% suppressMessages(), "data.frame")
