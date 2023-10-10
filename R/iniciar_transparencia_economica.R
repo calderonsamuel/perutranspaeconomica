67abb03f9d6730ca15c5962028ac417cdc2a8761
Samuel Calderon <samuel.calderon@uarm.pe>
2023-10-10 16:13:16 -0500

    change return value

--- R/iniciar_transparencia_economica.R
+++ R/iniciar_transparencia_economica.R
@@ -3,7 +3,7 @@
 #' @param actualizacion chr. Uno de c("mensual", "diaria")
 #' @param modulo chr. Uno de c("gasto", "ingreso")
 #' @param psize Cantidad máxima de filas del resultado. No cambiar a menos que se sepa con mucha seguridad lo que implica.
-#' @return Un data.frame con clase `<transpaeco>`
+#' @return Un objeto con clase `<transpaeco>`
 #' @export
 iniciar_transparencia_economica <- function(modulo = "gasto", actualizacion = "diaria", psize = 1000L) {
     transpaeco(modulo = modulo, actualizacion = actualizacion, psize = psize)
