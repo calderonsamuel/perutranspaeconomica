cb90d68c7e832223b2c66dc4dffb97b99653ff9d
Samuel Calderon <samuel.calderon@uarm.pe>
2026-04-25 17:10:36 -0500

    stop using dplyr::across() inside dplyr::filter()

--- R/process_tbl.R
+++ R/process_tbl.R
@@ -3,11 +3,22 @@ process_tbl <- function(tbl) {
     num_columns <- c("pia", "pim", "certificacion", "compromiso_anual",
                      "atencion_de_compromiso_mensual", "devengado", "girado", "avance_percent",
                      "recaudado")
-    tbl %>%
+    
+    tmp_tbl <- tbl %>%
         remove_first_column() %>%
-        unique() %>% # keep only unique permutations, returns data.frame
-        # dplyr::filter(!is.na(readr::parse_number(.data$pim))) %>%
-        dplyr::filter(dplyr::across(.cols = tidyselect::any_of(c("certificacion", "recaudado")), ~!is.na(readr::parse_number(.x)))) %>%
+        unique()# keep only unique permutations, returns data.frame
+    
+    # modulo "gasto" tiene "certification" y modulo "ingreso" tiene "recaudado"
+    if ("certificacion" %in% names(tmp_tbl)) {
+        tmp_tbl <- tmp_tbl %>%
+            dplyr::filter(!is.na(readr::parse_number(.data$certificacion)))
+    } else if ("recaudado" %in% names(tmp_tbl)) {
+        tmp_tbl <- tmp_tbl %>%
+            dplyr::filter(!is.na(readr::parse_number(.data$recaudado)))
+
+    }
+
+    tmp_tbl %>%
         separate_cod_desc() %>%
         dplyr::mutate(dplyr::across(.cols = tidyselect::any_of(num_columns), .fns = readr::parse_number)) %>%
         suppressWarnings()
